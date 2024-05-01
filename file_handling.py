7b281b073a68a478a04f53cd7ac8650bedc5335d
Junior's Signature <chibuzor.john.2018@gmail.com>
2024-05-01 12:08:50 +0100

    file handling

---
+++ file_handling.py
@@ -0,0 +1,24 @@
+import os
+
+file_name = 'data.txt'
+
+with open(file_name, 'r') as f:
+    file = f.read()
+    print(file)    
+    # for row in file:
+    #     print(row)
+
+file_name2 = 'none.txt'
+
+with open(file_name2, 'w') as w:
+    data = 'writing into file that does not exist\n'
+    w.write(data)
+
+with os.scandir('C:/Users') as entries:
+    for entry in entries:
+        print(entry.name)
+
+
+with open(file_name2, 'a') as a:
+    text_content = 'Welcome to Learn factory'
+    a.write(text_content)
